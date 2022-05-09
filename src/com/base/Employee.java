f557a9c73ffa17ff555b9df511ee8cf2547b0234
naveenupparatti <105036613+naveenupparatti@users.noreply.github.com>
2022-05-09 09:50:04 +0530

    Revert "New change in Emp1"

--- src/com/base/Employee.java
+++ src/com/base/Employee.java
@@ -3,8 +3,6 @@ package com.base;
 public class Employee {
 public static void main(String[] args) {
 	System.out.println("first change");
-	System.out.println("new change to master code");
 }
-
 
 }
