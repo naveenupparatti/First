290d77493f7125ed88fb9482b882d3ee45257c86
naveenupparatti <naveenupp00@gmail.com>
2022-05-09 09:47:49 +0530

    New change in Emp1

--- src/com/base/Employee.java
+++ src/com/base/Employee.java
@@ -3,6 +3,8 @@ package com.base;
 public class Employee {
 public static void main(String[] args) {
 	System.out.println("first change");
+	System.out.println("new change to master code");
 }
+
 
 }
